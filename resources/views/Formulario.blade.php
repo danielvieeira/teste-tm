e1b6bb6320b18fadc8d08218250cb7603c123a44
Daniel Vieira <danielvieeira@gmail.com>
2019-11-05 19:20:13 -0300

    Propriedades name adicionadas aos campos do formulario

--- resources/views/Formulario.blade.php
+++ resources/views/Formulario.blade.php
@@ -13,13 +13,14 @@
                 <div class="card-body">
                     <h4 class="card-title">Formulário de submissão</h4>
                     <form action="/" method="POST" class="col-sm-12">
+                        @csrf
                         <div class="form-group">
                             <label for="nome">Nome Completo:</label>
-                            <input type="text" class="form-control" id="nome" placeholder="Ex: João da Silva">
+                            <input name="nome" type="text" class="form-control" id="nome" placeholder="Ex: João da Silva">
                         </div>
                         <div class="form-group">
                             <label for="texto">Texto:</label>
-                            <textarea rows="4" class="form-control" id="texto" placeholder="Ex: A resposta para a vida, o universo e tudo mais."></textarea>
+                            <textarea name="texto" rows="4" class="form-control" id="texto" placeholder="Ex: A resposta para a vida, o universo e tudo mais."></textarea>
                         </div>
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary">Submeter</button>

96be4724756046f061f20b868e746cd33c232afe
Daniel Vieira <danielvieeira@gmail.com>
2019-11-06 16:01:49 -0300

    Cometários adicionados ao controlador da página principal.

--- app/Http/Controllers/PaginaController.php
+++ app/Http/Controllers/PaginaController.php
@@ -16,10 +16,15 @@ class PaginaController extends Controller
         // Recupera os dados passados no post
         $dados = $request->only('nome', 'texto');
 
+        /**
+         * Verifica se o nome contém menos de duas palavras.
+         * Caso sim, retorna o formulário com os dados submetidos e uma mensagem de erro
+         */
         if (str_word_count($dados['nome']) < 2) {
             return view('Formulario')->with('input', $dados)->withErrors('Nome incompleto');
         }
 
+        // Retorna o formulário com os dados submetidos
         return view('Formulario')->with('dados', $dados);
     }
 }
